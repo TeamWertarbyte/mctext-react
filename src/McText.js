d1a4e0542c9e28d0c490d433efc33b6b426ca285
leMaik <an@maik.tk>
2017-05-01 22:08:59 +0200

    Use prop-types package instead of React.PropTypes.

--- src/McText.js
+++ src/McText.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import defaultColormap from './defaultColormap'
 import convertTextToJson from './textToJson'
 import TextComponent from './TextComponent'
